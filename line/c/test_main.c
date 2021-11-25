639e08f576f6f60af8fac7f2ae2ffcc52c38a0f7
Rod Doe <doerodney@gmail.com>
2021-11-24 19:33:29 -0800

    Updated test_main.c to add test_line.

--- line/c/test_main.c
+++ line/c/test_main.c
@@ -4,6 +4,7 @@
 #include <setjmp.h>
 #include <cmocka.h>
 
+#include "test_line.h"
 #include "test_pt.h"
 
 int setup(void** state) { return 0; }
@@ -12,7 +13,8 @@ int tear_down(void** state) { return 0; }
 
 int main(int argc, char* argv[]) {
     const struct CMUnitTest tests[] = {
-        cmocka_unit_test(test_xypoint)
+        cmocka_unit_test(test_xypoint),
+        cmocka_unit_test(test_line)
     };
 
     return cmocka_run_group_tests(tests, setup, tear_down);
