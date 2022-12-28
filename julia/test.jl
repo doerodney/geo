fa497518c2c5cb068b6bee539af589c32b679e9c
Rod Doe <doerodney@gmail.com>
2022-12-28 15:56:12 -0800

    Added test.jl crash test dummy code.

---
+++ julia/test.jl
@@ -0,0 +1,63 @@
+import GSL
+using Printf
+
+
+struct XYPt
+    x::Float64
+    y::Float64
+    XYPt(x, y) = new(x, y)
+end
+
+
+function my_f(vars::Ptr{GSL.C.gsl_vector}, params::Ptr{GSL.C.gsl_vector})
+    varg = GSL.wrap_gsl_vector(vars)
+    @show varg
+
+    parg = GSL.wrap_gsl_vector(params)
+    @show parg
+end
+
+
+function main()
+    n = 2
+    v = GSL.vector_alloc(2)
+    m = 1.414
+    b = 1.732
+    GSL.vector_set(v, 0, m)
+    GSL.vector_set(v, 1, b)
+    @show v
+
+    p = GSL.vector_alloc(n)
+    for i in 1:n
+        GSL.vector_set(p, i - 1, i * 2.0)
+    end
+    @show p
+
+    # params = convert(Ptr{Nothing}, p)
+
+    my_f(v, p)
+end
+
+main()
+
+# static double my_f(const gsl_vector *v, void *params) {
+#   double m = gsl_vector_get(v, 0);
+#   double b = gsl_vector_get(v, 1);
+#   XYPointList *pts = (XYPointList*) params;
+
+#   double diff = 0.0;
+#   double err = 0.0;
+#   XYPoint pt;
+#   int failure;
+#   for (int i = 0; i < pts->count; i++) {
+#     failure = XYPointList_get(pts, i, &pt);
+#     if (failure) {
+#       break;
+#     } else {
+#       diff = (pt.y - (m * pt.x) - b);
+#       err += diff * diff;
+#     }
+#   }
+
+#   return err;
+# }
