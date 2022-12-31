a605a594451f4ed65748a88b39d3c58b865a15f8
Rod Doe <doerodney@gmail.com>
2022-12-31 07:38:20 -0800

    Interim commit.

--- julia/fitline.jl
+++ julia/fitline.jl
@@ -7,6 +7,14 @@ struct XYPt
     XYPt(x, y) = new(x, y)
 end
 
+struct FitLineResult {
+    b::Cdouble
+    m::Cdouble
+    x::Cdouble
+    vertical::Cint
+    failure::Cint
+}
+
 function my_f(_v::Ptr{GSL.C.gsl_vector}, _params::Ptr{Cvoid})::Cdouble
     err::Cdouble = 0.0
     
@@ -35,6 +43,7 @@ function my_f(_v::Ptr{GSL.C.gsl_vector}, _params::Ptr{Cvoid})::Cdouble
     @show err
     return err
 end
+
 
 function my_df(_v::Ptr{GSL.C.gsl_vector}, _params::Ptr{Cvoid}, _df::Ptr{GSL.C.gsl_vector})::Cvoid
     dedm::Cdouble = 0.0
@@ -77,61 +86,63 @@ function my_fdf(_v::Ptr{GSL.C.gsl_vector}, _params::Ptr{Cvoid}, f::Ptr{Cdouble},
 end
 
 
-#--main--
-n = 2
+function FitLine(_params::Ptr{GSL.C.gsl_vector}, step_size::Cdouble, tol::Cdouble)
+    iter = 0
+    err::Cint = 0
 
-# Initial values of (m, b):
-v = GSL.vector_alloc(2)
-m = 2.0
-b = 1.0
-GSL.vector_set(v, 0, m)
-GSL.vector_set(v, 1, b)
+    # Initial values of (m, b):
+    v = GSL.vector_alloc(2)
+    m = 1.0
+    b = 0.0
+    GSL.vector_set(v, 0, m)
+    GSL.vector_set(v, 1, b)
 
-# Generate data points for a different (m, b) that is not 
-# equal to the initial guess of (m, b):
-m_gen = 3.0
-b_gen = 4.0
+    # Convert data points vector to void pointer for library:
+    params = convert(Ptr{Cvoid}, _params)
 
-count = 10
-p = GSL.vector_alloc(count * 2)
-idx = 0
+    # Create/init minimizer struct:
+    my_func::GSL.gsl_multimin_function_fdf
+    my_func.n = 2           # number of function components
+    my_func.f = my_f        # v error function    
+    my_func.df = my_df      # gradient function  
+    my_func.fdf = my_fdf    # error and gradient function
+    my_func.params = params # data
 
-# x,y points are put into the vector in Xn, Yn, Xn+1, Yn+1 ... order:
-for x in 1:count
-    # x:
-    GSL.vector_set(p, idx, x)
-    idx += 1
-    # Add random noise to y:
-    noise = rand(-10.0:10.0)/100.0
-    y = m_gen * x + b_gen + noise
+    state = GSL.gsl_multimin_fdfminizer_alloc(GSL.gsl_multimin_fdfminimizer_conjugate_fr, 2)
+    GSL.gsl_multimin_fdf_minimizer_set(state, my_func, v, step_size, tol)
 
-    GSL.vector_set(p, idx, y)
-    idx += 1
+    # TODO:  
+    # Add iteration loop
+    # Add return value
 end
 
-params = convert(Ptr{Cvoid}, p)
 
+function main()
+    step_size::Cdouble = 0.01
+    tol::Cdouble = 0.0001
 
-# xypts = generate_points(10, 2, 0)
-# err = get_error(xypts, 2, 0)
-# @printf("Error = %g for m = %g, b = %g\n", err, 2, 0)
+    # Generate data points for a different (m, b) that is not 
+    # equal to the initial guess of (m, b):
+    m_gen = 3.0
+    b_gen = 4.0
 
+    count = 10
+    p = GSL.vector_alloc(count * 2)
+    idx = 0
 
-# my_func = GSL.gsl_multimin_function_fdf
+    # x,y points are put into the vector in Xn, Yn, Xn+1, Yn+1 ... order:
+    for x in 1:count
+        # x:
+        GSL.vector_set(p, idx, x)
+        idx += 1
+        # Add random noise to y:
+        noise = rand(-10.0:10.0)/100.0
+        y = m_gen * x + b_gen + noise
 
-# my_func.
+        GSL.vector_set(p, idx, y)
+        idx += 1
+    end
 
-# my_func.n = 2;                  /* number of function components */
-# my_func.f = &my_f;              /* error function */   
-# my_func.df = &my_df;            /* gradient function */  
-# my_func.fdf = &my_fdf;          /* error and gradient function */
-# my_func.params = (void *) pts;  /* data */
-# # println(xypts)
-# m = 1
-# b = 1
-# err = get_error(xypts, m, b)
-# @printf("Error = %g for m = %g, b = %g\n", err, m, b)
-# step = 0.01
-# count = 100
-# epsilon = 0.01
-# line_gradient_descent(xypts, 1, 0, step, count, epsilon)
+    params = convert(Ptr{Cvoid}, p)
+
+    
